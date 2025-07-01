7f22c2e6dd28ec2503087874b170dcf7bbf75811
Özgür Efe Erturan <147487990+ozgurert@users.noreply.github.com>
2025-07-01 10:32:06 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://staj-projem-frontend-b7gufmg4bvcgcxbu.polandcentral-01.azurewebsites.net' // Canlı backend adresiniz
+  apiUrl: 'staj-projem-bdcjfce7gyhrfdbw.polandcentral-01.azurewebsites.net' // Canlı backend adresiniz
 };
